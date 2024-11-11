6c5ec2a98dfa60bb6e3aab3d6ab30637809c7d15
habebaya <habebayasser13@gmail.com>
2024-11-11 12:43:10 +0200

    change video

--- lib/main.dart
+++ lib/main.dart
@@ -11,13 +11,14 @@ class MyApp extends StatelessWidget {
     return  MaterialApp(
         title: 'Turn Digital',
         debugShowCheckedModeBanner: false,
-        home: InteractiveViewer(     // enable Zoom
-                // Set limits for zoom
-                minScale: 0.5,
-                maxScale: 2.5,
-                boundaryMargin: EdgeInsets.zero,
-                child:
-                MainScreen()));
+        // home: InteractiveViewer(     // enable Zoom
+        //         // Set limits for zoom
+        //         minScale: 0.5,
+        //         maxScale: 2.5,
+        //         boundaryMargin: EdgeInsets.zero,
+        //         child:
+           home:      MainScreen());
+    //);
     //);
   }
 }
